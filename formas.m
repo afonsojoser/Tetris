93b72c69f0ca4b0914bb60ac58bec7578fd77b1e
Utilizador <Utilizador@DESKTOP-BKDKEQK>
2026-04-27 13:07:42 +0100

    .

--- formas.m
+++ formas.m
@@ -9,7 +9,7 @@ hold on;
 view(3); grid on; axis equal;
 
 % Vértices e faces padrão de um cubo unitário
-v_base = [x y z; x+1 y z; x+1 y+1 z; x y+1 z; x y z+1; x+1 y z+1; x+1 y+1 z+1; x y+1 z+1];
+v_base = [0 0 0; 1 0 0; 1 1 0; 0 1 0; 0 0 1; 1 0 1; 1 1 1; 0 1 1];
 f = [1 2 6 5; 2 3 7 6; 3 4 8 7; 4 1 5 8; 1 2 3 4; 5 6 7 8];
 
 switch n
