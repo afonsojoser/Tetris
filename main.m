9db6cf61ef972101a35dd6e94b491e470f23ea2c
MCura11 <miguel.alexandre.cura@gmail.com>
2026-05-08 20:25:38 +0100

    Divisão em pastas das funções

--- main.m
+++ main.m
@@ -3,7 +3,11 @@ clear, clc, format compact,close all;
 
 n = 5;
 h = 10;
-%
-
+%adiciona as pastas
+caminhobase=fileparts(mfilename('fullpath'));
+addpath(fullfile(caminhobase,'ficheiros'));
+addpath(fullfile(caminhobase,'imagens'));
+addpath(fullfile(caminhobase,'jogos'));
+addpath(fullfile(caminhobase,'menus'));
 % abre o ecrã do jogo
 menu(n, h)
